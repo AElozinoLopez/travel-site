b83f779452664acd965b02c2efffd8feec944c9f
ElozinoLopez <asaijeelozinolopez@gmail.com>
2024-10-03 21:30:48 +0100

    Created styles directory and a style.css file within it

--- app/bundled.js
+++ app/bundled.js
@@ -15,7 +15,7 @@
   \***********************************/
 /***/ (() => {
 
-eval("alert(\"Hello, this is just a test!!!\")\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
+eval("alert(\"Hello, this is just the beginning of great things!\")\n\n//# sourceURL=webpack://travel-site/./app/assets/scripts/App.js?");
 
 /***/ })
 
