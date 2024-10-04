03ce540692f4944f557146e4c653bd0b2582f400
ElozinoLopez <asaijeelozinolopez@gmail.com>
2024-10-04 23:55:11 +0100

    Made css-loader an object in webpack.config so I can manually manage my images

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,  //This tells webpack to treat any file with .css extension
-                use: ['style-loader', 'css-loader', {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]  //This allows webpack to apply the style to the browser and bundle the css file respectively.
+                //use: ['style-loader', 'css-loader', {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]  //This allows webpack to apply the style to the browser and bundle the css file respectively.
+                use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]   //The essence of css-loader in this fashion is to allow us manually manage out images
             }
         ]
     }
