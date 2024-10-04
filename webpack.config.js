fd78239ec230a9e2327e5904beca7a877bf967cf
ElozinoLopez <asaijeelozinolopez@gmail.com>
2024-10-04 23:05:14 +0100

    Added a comma after postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path')
 
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
 module.exports = {
     entry: './app/assets/scripts/App.js',
     output: {
@@ -12,7 +18,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,  //This tells webpack to treat any file with .css extension
-                use: ['style-loader', 'css-loader', {loader: "postcss-loader" options: {postcssOptions: {plugins: postCSSPlugins}}}]  //This allows webpack to apply the style to the browser and bundle the css file respectively.
+                use: ['style-loader', 'css-loader', {loader: "postcss-loader", options: {postcssOptions: {plugins: postCSSPlugins}}}]  //This allows webpack to apply the style to the browser and bundle the css file respectively.
             }
         ]
     }
