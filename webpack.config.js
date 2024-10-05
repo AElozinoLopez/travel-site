e11aaf30ca0a2116b9b2f9f7305dbe2e70ccfa30
ElozinoLopez <asaijeelozinolopez@gmail.com>
2024-10-05 20:39:11 +0100

    Installed webpack-dev-server and added code to point webpack to the directory to serve up

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "app")
+        }
+    },
     mode: 'development',
     watch: true,
     module: {
